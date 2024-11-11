74e3361995034ef5579e863bcf38335651ce90dc
matteo <matteo.massimello@gmail.com>
2024-11-11 22:55:40 +0100

    not progress

--- to-do.md
+++ to-do.md
@@ -9,3 +9,5 @@
 write down ↓ my daily-do goals when i do this project
 
 # little acvment to do daily
+
+[ ] create a canvas over the other canvas
--- web/js/index.js
+++ web/js/index.js
@@ -11,22 +11,38 @@ const dimension = {
 let ctx = [];
 
 function init() {
-    let cont = document.getElementById("container");
+    let cont = document.getElementById("tableGame");
     let position = 0
 
     for (let i = 0; i < dimension.rows; i++) {
+        cont.innerHTML += "<tr>"
+
         for (let j = 0; j < dimension.collo; j++) {
-            cont.innerHTML += '<canvas class="canvas" id="canvas" onmouseleave="backColor(' + position + ')" onmouseenter="changeColor(' + position + ')">Sorry, bet no canvas</canvas>';
+            cont.innerHTML += '<th><canvas class="canvas" id="canvas" onmouseleave="backColor(' + position + ')" onmouseenter="changeColor(' + position + ')">Sorry, bet no canvas</canvas></th>';
             ++position;
         }
+
+        cont.innerHTML += "</tr>"
     }
 
     const canvas = document.querySelectorAll("canvas");
 
     for (let i = 0; i < dimension.rows * dimension.collo; i++) {
         ctx[i] = canvas[i].getContext("2d");
+        if (i === 0) {
+            drawnChar(i, 0, 0);
+        }
         square(ctx[i]);
     }
+}
+
+function drawnChar(i, x, y) {
+    console.log("enter");
+    ctx[i].rect(x, y, 100, 100);
+    ctx[i].lineWidth = 1;
+    ctx[i].stroke();
+    ctx[i].fillStyle = "black";
+    ctx[i].fill();
 }
 
 function square(ctx) {
