6c1857806630cabdfca75734576aa34e9073596b
matteo <matteo.massimello@gmail.com>
2024-11-12 22:24:53 +0100

    done the hero, now to make all center (this was only experimental)

--- web/js/index.js
+++ web/js/index.js
@@ -3,7 +3,13 @@ let position = {
     y: 0
 };
 
-const dimension = {
+const dimensionCanvs = {
+    width: 50,
+    height: 50
+
+}
+
+const dimensionGrid = {
     rows: 10,
     collo: 10
 }
@@ -11,43 +17,43 @@ const dimension = {
 let ctx = [];
 
 function init() {
-    let tableGame = document.getElementById("tableGame");
     let position = 0
 
-    for (let i = 0; i < dimension.rows; ++i) {
-        const tr = tableGame.insertRow();
-        for (let j = 0; j < dimension.collo; ++j) {
-            //cont.innerHTML += '<td><canvas class="canvas" id="canvas" onmouseleave="backColor(' + position + ')" onmouseenter="changeColor(' + position + ')">Sorry, bet no canvas</canvas></td>';
-            const td = tr.insertCell();
-            td.innerHTML = '<canvas class="canvas" id="canvas' + position + '" onmouseleave="backColor(' + position + ')" onmouseenter="changeColor(' + position + ')">Sorry, bet no canvas</canvas>';
-            ++position;
-        }
+    let posl = 0;
+    let post = 0;
 
+    for (let i = 0; i < dimensionGrid.rows; ++i) {
+        for (let j = 0; j < dimensionGrid.collo; ++j) {
+            document.getElementById('container').innerHTML += '<canvas class="canvas" id="canvas' + position + '"style="position: center; margin: 0px; padding: 0px;" onmouseleave="backColor(' + position + ')" onmouseenter="changeColor(' + position + ')">Sorry, bet no canvas</canvas>';
+            ++position;
+            posl += dimensionCanvs.width;
+        }
+        posl = 0;
+        post += dimensionCanvs.height;
     }
 
 
+
+    document.getElementById('container').innerHTML += '<canvas class="hero" id="hero" style="position: absolute; left: 0px; top: 0px; width: 20px; height: 20px;">Sorry no</canvas>';
     const canvas = document.querySelectorAll("canvas");
 
-    for (let i = 0; i < dimension.rows * dimension.collo; i++) {
+
+    for (let i = 0; i < dimensionGrid.rows * dimensionGrid.collo; ++i) {
         ctx[i] = canvas[i].getContext("2d");
         square(ctx[i]);
     }
 
     /*FIX: use this the hero do not appear */
-    /*TODO: do this ↓ but for all the canvas */
 
-    document.getElementById('canvas0').style.position = "absolute";
-    document.getElementById('canvas0').style.left = 0;
-    document.getElementById('canvas0').style.top = 0;
 
-    hero = document.getElementById('hero');
+    const hero = document.getElementById('hero').getContext("2d");
 
     drawnChar(hero, 0, 0);
 }
 
 function drawnChar(hero, x, y) {
     console.log("enter");
-    hero.rect(x, y, 100, 100);
+    hero.rect(x, y, dimensionCanvs.width * 4, dimensionCanvs.height * 4);
     hero.lineWidth = 1;
     hero.stroke();
     hero.fillStyle = "black";
@@ -55,7 +61,7 @@ function drawnChar(hero, x, y) {
 }
 
 function square(ctx) {
-    ctx.rect(position.x, position.y, 300, 300);
+    ctx.rect(position.x, position.y, dimensionCanvs.width * 6, dimensionCanvs.height * 6);
     ctx.lineWidth = 1;
     ctx.stroke();
     ctx.fillStyle = "teal";
@@ -68,7 +74,7 @@ function backColor(position) {
 }
 
 function changeColor(position) {
-    ctx[position].fillStyle = "black";
+    ctx[position].fillStyle = "white";
     ctx[position].fill();
 }
 
