d0f3aa2441ba32fac8c5aca71d2509738984dff5
matteo <matteo.massimello@gmail.com>
2024-11-15 14:49:22 +0100

    done the move of the hero and a pice of an arena

--- web/js/index.js
+++ web/js/index.js
@@ -7,51 +7,71 @@ let canvs = {
     }
 }
 
+let theHero = {
+    hero: null,
+    ctxh: null,
+    postion: {
+        x: 0,
+        y: 8
+    }
+};
+
 function init() {
-
     Arena();
-
-
-    const hero = document.getElementById("hero");
-    const ctxh = hero.getContext("2d");
-    ctxh.beginPath();
-    ctxh.fillStyle = "#FF0000";
-    ctxh.fillRect(69, 69, 20, 20);
-    ctxh.stroke();
-
+    Hero();
 }
 
 function Arena() {
     let container = document.getElementById("container");
-    console.log(container);
     let dimension = 0;
 
     for (let i = 0; i < 5; ++i) {
         for (let j = 0; j < 5; ++j) {
-            container.appendChild('<canvas id="arena">Sorry no cavs for you</canvas>');
+            let theArena = document.createElement('canvas');
+            theArena.innerHTML = "Sorry no cavs for you";
+            theArena.setAttribute('id', 'arena');
+            container.appendChild(theArena);
             canvs.position.x[dimension] = (42 * j);
             canvs.position.y[dimension] = (42 * i);
             ++dimension;
         }
     }
 
-    let temp = document.querySelectorAll("arena");
+    let temp = container.querySelectorAll('canvas');
 
     for (let i = 0; i < 25; i++) {
-        console.log(temp);
         canvs.element[i] = temp[i];
-        console.log(canvs.element[i]);
         canvs.ctx[i] = canvs.element[i].getContext("2d");
-        drawArena(canvs.ctx[i], canvs.position.x[i], canvs.position.y[i]);
+        // drawArena(canvs.ctx[i], canvs.position.x[i], canvs.position.y[i]);
+        drawArena(canvs.ctx[i], 0, 0);
     }
 
 }
 
 function drawArena(ctx, x, y) {
-    console.log(x, y, ctx);
+    console.log(x, y);
     ctx.beginPath();
     ctx.fillStyle = "#00FF00";
     ctx.fillRect(x, y, 420, 420);
     ctx.stroke();
 }
 
+function Hero() {
+    theHero.hero = document.getElementById("hero");
+    theHero.ctxh = theHero.hero.getContext("2d");
+
+    theHero.ctxh.beginPath();
+    theHero.ctxh.arc(170, 75, 73, 0, 10);
+    theHero.ctxh.fillStyle = "#FF0000";
+    theHero.ctxh.fill();
+    theHero.ctxh.stroke();
+
+    theHero.hero.style.left = theHero.postion.x + "px"
+    theHero.hero.style.top = theHero.postion.y + "px"
+}
+
+function moveHero() {
+    console.log("Entro");
+    theHero.postion.x += 10
+    theHero.hero.style.left = theHero.postion.x + "px";
+}
