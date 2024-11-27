99c9af7a9c6e758082fdf675e65c15d8d40e9dc8
matteo <matteo.massimello@gmail.com>
2024-11-27 10:59:37 +0100

    make the portal in the most distant position

--- src/wasm/createArena/portalCreate.go
+++ src/wasm/createArena/portalCreate.go
@@ -1,20 +1,35 @@
 package createarena
 
 import (
-	"math/rand"
 	"syscall/js"
 )
 
-//todo: maybe create the portal in the more distant place from the start
-
-func createPortaleWin() {
-	value := 0
-
-	for Path.ArrayPath[value] == Path.ArrayPath[0] {
-		value = rand.Intn(len(Path.ArrayPath))
+func assoluteValue(num int) int {
+	if num < 0 {
+		return (num * -1)
 	}
 
-	Path.Path[Path.ArrayPath[value]].Portal = true
+	return num
+}
+
+func createPortaleWin() {
+
+	maxDistance := -1
+	saveKey := ""
+
+	for key, elem := range Path.Path {
+
+		num1 := assoluteValue(Path.Path[Path.ArrayPath[0]].Number1 - elem.Number1)
+		num2 := assoluteValue(Path.Path[Path.ArrayPath[0]].Number2 - elem.Number2)
+
+		if (num1 + num2) > maxDistance {
+			saveKey = key
+			maxDistance = (num1 + num2)
+		}
+
+	}
+
+	Path.Path[saveKey].Portal = true
 
 }
 
